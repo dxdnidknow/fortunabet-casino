4fc91366845ec854093c3fa628df8f2fab21dd03
Fortunabet <zambranodaniel046@gmail.com>
2025-12-22 23:02:34 -0400

    test 3.3

--- js/config.js
+++ js/config.js
@@ -1,1 +1,5 @@
-export const API_BASE_URL = 'https://fortunabet-api.onrender.com/api'; // <-- ¡ASEGÚRATE DE QUE ESTÉ ASÍ!
+// Modo Producción (Render)
+export const API_BASE_URL = 'https://fortunabet-api.onrender.com/api'; 
+
+// Modo Local
+// export const API_BASE_URL = 'http://localhost:5000/api';
